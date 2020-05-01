6d99dcf150af8b1f577bf8d061806fb96009b551
Agata Zurek <agata.w.zurek@gmail.com>
2020-04-30 20:51:25 -0700

    added code comments and function descriptions

--- js/script.js
+++ js/script.js
@@ -25,6 +25,9 @@ const ccNumInput = document.getElementById('cc-num');
 const zipInput = document.getElementById('zip');
 const cvvInput = document.getElementById('cvv');
 
+/**
+ * Object of objects for the input validation requirements and associated tip text
+ */
 const validators = {
 	name  : {
 		regex         : /^[a-z]+$/i,
@@ -51,10 +54,14 @@ const validators = {
 	}
 };
 
+/**
+ * Setup initial form view
+ */
 function init() {
+	// add event listeners to inputs for validation
 	setupInputListeners(validators);
+	// hide job role - other
 	otherJobRoleInput.style.display = 'none';
-	setShirtColorOptions();
 	// hide span that displays the total
 	activitySpan.classList.add('hide');
 	// select credit card payment method as default
@@ -67,9 +74,9 @@ function init() {
 
 init();
 
-/* ”Job Role” section */
+/* ------ ”Job Role” section ------ */
+// add event listener to job role select to show/hide 'other' field
 userTitleSelect.addEventListener('change', function() {
-	// if other was selected, show other input field
 	if (this.value === 'other') {
 		otherJobRoleInput.style.display = '';
 	} else {
@@ -77,44 +84,50 @@ userTitleSelect.addEventListener('change', function() {
 	}
 });
 
-/* ”T-Shirt Info” section */
+/* ------ ”T-Shirt Info” section ------ */
+/**
+ * limits the t-shirt color options to only show those corresponding to the selected design
+ * @param {string} design string specifying which design set to show
+ */
 function setShirtColorOptions(design = 'none') {
+	// T-shirt color select element
 	const shirtColors = document.getElementById('color');
+	// boolean flag to select first t-shirt color of design set
 	let selectedFlag = false;
 
 	for (let i = 0; i < shirtColors.options.length; i++) {
 		if (shirtColors.options[i].innerText.includes(design)) {
+			// show select options of the selected design
 			shirtColors.options[i].hidden = false;
+			// select the first t-shirt color of the new set so the user doesn't see an invalid option
 			if (!selectedFlag) {
 				shirtColors.options[i].selected = true;
 				selectedFlag = true;
 			}
 		} else {
+			// hide and unselect inapplicable color options
 			shirtColors.options[i].hidden = true;
 			shirtColors.options[i].selected = false;
 		}
 	}
+	// if no design selected, hide the color select and change its label
+	// otherwise show the color select, change its label and set focus on it
 	if (design === 'none') {
-		// if a design isn't selected, hide all the colors and the color dropdown
-		// and change the label to tell the user to pick a design
 		shirtColors.previousElementSibling.textContent = 'Please select a T-shirt theme';
 		shirtColors.hidden = true;
 	} else {
-		// change the label for the color dropdown
 		shirtColors.previousElementSibling.textContent = 'Color:';
-		// show the color dropdown
 		shirtColors.hidden = false;
-		// set focus to the color dropdown
 		shirtColors.focus();
 	}
 }
 
+// add event listener to t-shirt design dropdown to call function
+// that displays corrsponding t-shirt colors
 shirtDesignSelect.addEventListener('change', function() {
 	switch (this.value) {
 		case 'js puns':
-			// display js pun shirt options
 			setShirtColorOptions('Puns');
-			// show color dropdown
 			break;
 		case 'heart js':
 			setShirtColorOptions('I');
@@ -125,15 +138,53 @@ shirtDesignSelect.addEventListener('change', function() {
 	}
 });
 
-/* ”Register for Activities” section */
-// add event listener to all the checkboxes
+/* ------ ”Register for Activities” section ------ */
+/**
+ * Looks through all activities to disable any that are at a conflicting day/time
+ */
+function disableConflictingActivities() {
+	const checkedDayAndTime = event.target.dataset.dayAndTime; // day/time of checked activity
+	const checkboxName = event.target.name; // name attribute of checked activity
+	if (checkedDayAndTime) {
+		// loop through all checkboxes to see if there is a matching day/time for any of the other activities
+		for (let i = 0; i < activityCheckboxes.length; i++) {
+			if (
+				checkboxName !== activityCheckboxes[i].name && // don't compare the day/time of the checkbox that triggered event
+				checkedDayAndTime === activityCheckboxes[i].dataset.dayAndTime
+			) {
+				// disable conflicting activity and style
+				activityCheckboxes[i].disabled = !activityCheckboxes[i].disabled;
+				activityCheckboxes[i].parentElement.classList.toggle('disabled');
+			}
+		}
+	}
+}
+/**
+ * calculates the total cost of the selected activities
+ * @returns {number}
+ */
+function totalCost() {
+	let total = 0;
+	// loop over checked activites and sum up the total
+	for (let i = 0; i < activityCheckboxes.length; i++) {
+		if (activityCheckboxes[i].checked) {
+			total += +activityCheckboxes[i].dataset.cost;
+		}
+	}
+	return total;
+}
+// add event listener to all the checkboxes to validate activities
+// selected and keep a running total of costs
 for (let i = 0; i < activityCheckboxes.length; i++) {
 	activityCheckboxes[i].addEventListener('change', () => {
-		// add checked class
+		// style checked activities
 		event.target.parentElement.classList.toggle('checked');
+
 		disableConflictingActivities();
+
 		//update the total cost when user checks/unchecks activities
 		const cost = totalCost();
+
 		// update form to display total cost
 		if (cost > 0) {
 			activitySpan.textContent = `Your total cost: $${cost}`;
@@ -146,111 +197,114 @@ for (let i = 0; i < activityCheckboxes.length; i++) {
 	});
 }
 
-function disableConflictingActivities() {
-	// search through options and if there is a match to the checkbox that was just changed, toggle disabled
-	const checkedDayAndTime = event.target.dataset.dayAndTime;
-	const checkboxName = event.target.name;
-	if (checkedDayAndTime) {
-		// loop through all checkboxes to see if there is a match
-		for (let i = 0; i < activityCheckboxes.length; i++) {
-			if (
-				checkboxName !== activityCheckboxes[i].name &&
-				checkedDayAndTime === activityCheckboxes[i].dataset.dayAndTime
-			) {
-				activityCheckboxes[i].disabled = !activityCheckboxes[i].disabled;
-				activityCheckboxes[i].parentElement.classList.toggle('disabled');
-			}
-		}
-	}
-}
-
-function totalCost() {
-	let total = 0;
-	// loop over checked activites and sum up the total
-	for (let i = 0; i < activityCheckboxes.length; i++) {
-		if (activityCheckboxes[i].checked) {
-			total += +activityCheckboxes[i].dataset.cost;
-		}
-	}
-	return total;
-}
-
-/* "Payment Info" section */
-paymentSelect.addEventListener('change', () => {
-	const selected = paymentSelect.value.replace(' ', '-');
-	displayPayment(selected);
-});
-
+/* ------ "Payment Info" section ------ */
+/**
+ * Shows just the selected payment option section
+ * @param {string} paymentOption a value from the select payment dropdown
+ */
 function displayPayment(paymentOption) {
+	// first hide all the payment option sections
 	for (let i = 1; i < paymentSelect.options.length; i++) {
 		const option = paymentSelect.options[i].value.replace(' ', '-');
 		document.getElementById(option).style.display = 'none';
 		document.getElementById(option).classList.add('hide');
 	}
+	// then show just the selected payment option section
 	document.getElementById(paymentOption).style.display = '';
 	document.getElementById(paymentOption).classList.remove('hide');
 }
-/* Form Validation */
 
-function activityChecked() {
-	for (let i = 0; i < activityCheckboxes.length; i++) {
-		if (activityCheckboxes[i].checked) {
-			return true;
-		}
-	}
-	// show error message
-	activitySpan.textContent = 'Please select at least one activity';
-	activitySpan.classList.add('invalid');
-	activitySpan.classList.remove('hide');
-}
+// add event listener to payment dropdown
+paymentSelect.addEventListener('change', () => {
+	const selected = paymentSelect.value.replace(' ', '-');
+	displayPayment(selected);
+});
 
-// code modified from Treehouse Regex Course
+/* ------ Form Validation ------ */
+/**
+ * I don't know how to document this, advice appreciated!
+ * Takes an object of validation information and returns an anonymous
+ * function that will be assigned to an 'input' event listener
+ * code modified from Treehouse Regex Course
+ * @param {object} validator 
+ */
 function createListener(validator) {
 	return event => {
-		const targetElem = event.target;
-		const text = targetElem.value;
-		const label = targetElem.previousElementSibling;
-		const showEmptyTip = !text;
-		const showBadPatternTip = !validator.regex.test(text);
-		const emptyTip = validator.emptyTip;
-		const badPatternTip = validator.badPatternTip;
+		const targetElem = event.target; // element that triggered the event
+		const text = targetElem.value; // user input in target element
+		const label = targetElem.previousElementSibling;	// label for target element
+		const showEmptyTip = !text;	// boolean: true if empty; false otherwise
+		const showBadPatternTip = !validator.regex.test(text); // boolean: result of regex test on user input, true if false
+		const emptyTip = validator.emptyTip; // tip to show user if field is empty
+		const badPatternTip = validator.badPatternTip; // tip to show user if regex failed
 
-		// reset
+		// reset validation message and style
 		label.removeAttribute('data-tip');
 		label.classList.remove('invalid');
+
 		if (showEmptyTip) {
+			// if input field is empty, notify user it is required
 			if (emptyTip) targetElem.placeholder = emptyTip;
 			targetElem.classList.add('invalid');
 		} else if (showBadPatternTip) {
+			// if user input failed validation regex pattern, notify user
 			if (badPatternTip) label.setAttribute('data-tip', badPatternTip);
 			targetElem.classList.add('invalid');
 			label.classList.add('invalid');
 		} else {
+			// input ok, remove all validation notifications
 			label.removeAttribute('data-tip');
 			targetElem.placeholder = '';
 			targetElem.classList.remove('invalid');
 		}
 	};
 }
-
+/**
+ * adds event listner for 'input' event on all inputs with validation requirements
+ * @param {object} validators 
+ */
 function setupInputListeners(validators) {
+	// loop through all inputs on form
 	for (let i = 0; i < inputs.length; i++) {
+		// find id attribute: will match to validators object keys
+		// remove hyphen from id as object keys cannot contain special characters
 		const id = inputs[i].id.replace('-', '');
 		if (validators[id]) {
+			// if input has required validation parameters, add event listener to input
 			inputs[i].addEventListener('input', createListener(validators[id]));
 		}
 	}
 }
 
-function checkIfInvalid(elem) {
-	return elem.classList.contains('invalid');
-}
-
+// add event listner to form to validate data on submit
 form.addEventListener('submit', () => {
-	let isValid = true;
-	let scrollTo;
+	/**
+	 * Checks given element for a class attribute indicating invalid input
+	 * @param {element} elem 
+	 */
+	function checkIfInvalid(elem) {
+		return elem.classList.contains('invalid');
+	}
+	/**
+ * Looks through all the activity checkboxes to see if at least one has been checked
+ * notifies user if none are checked
+ */
+	function activityChecked() {
+		for (let i = 0; i < activityCheckboxes.length; i++) {
+			if (activityCheckboxes[i].checked) {
+				return true;
+			}
+		}
+		// show error message
+		activitySpan.textContent = 'Please select at least one activity';
+		activitySpan.classList.add('invalid');
+		activitySpan.classList.remove('hide');
+	}
+	// boolean to say the form is valid. Starts as true and will be set false if any validation requirement failed
+	let isValid = true;  
+	let scrollTo; // Position on form to scroll to if input is invalid
 
-	// manually trigger all the input events
+	// manually trigger all the input events just in case user did not enter a required field
 	for (let i = 0; i < inputs.length; i++) {
 		inputs[i].dispatchEvent(new Event('input'));
 	}
